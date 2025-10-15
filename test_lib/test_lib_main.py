f82f44c5c403a048ede662722864f002ba990caa
ViniciusCunha2805 <viniciussilvacunha2805@gmail.com>
2025-10-15 10:39:29 -0300

    feat: troquei nome do folder

--- test_lib/test_lib_main.py
+++ test_lib/test_lib_main.py
@@ -1,5 +1,5 @@
 import pytest
-from calculator-vini-lib.operations import add, subtract, multiply, divide
+from calculator_vini_lib.operations import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
